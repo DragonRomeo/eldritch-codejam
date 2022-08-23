e996b80fe3a9e751400e5b6dc983d4d564cf7613
Ilya Dragonromeo <you@example.com>
2022-08-23 23:33:35 +0300

    feat: add ancientSelection

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 import cardsData from "./data/mythicCards/blue/index2.js";
+import ancientsData from "./data/ancients.js";
 
 
 const ancientGods = document.querySelector('.ancient-gods');
+const ancientItems = document.querySelectorAll('.ancient-item')
+const ancientGodsArray = Array.from(ancientItems);
 const difficultyContainer = document.querySelector('.difficulty-container');
 const lastCard = document.querySelector('.last-card');
 
-// lastCard.style.backgroundImage = "url('../assets/MythicCards/blue/blue1.png')";
 lastCard.style.backgroundImage = `url(${cardsData[0].cardFace})`;
 
 
@@ -14,18 +16,43 @@ const ancientToggleClass = (event) => {
 
     if (target.classList.contains('ancient-item')) {
         target.classList.toggle('active');
-        console.log('переключил');
-        console.log(`индес элемента = ${ancientGods.indexOf()}`)
+        // console.log('переключил');
+        // console.log(`indexOf =${ancientGodsArray.indexOf(target)}`) ;
         if (target.classList.contains('active')) {
-            console.log(`содержит актив`);
+            // console.log(`содержит актив`);
             ancientSelection(target);
         }
     }
 }
 
 const ancientSelection = (target) => {
+    console.log(`indexOf =${ancientGodsArray.indexOf(target)}`);
+    const dots = document.querySelectorAll('.dot');
+    console.log(dots);
+    let index = ancientGodsArray.indexOf(target);
+    console.log(`ancientGodIndex = ${index}`);
+    const stage1 = [ancientsData[index].firstStage.greenCards, ancientsData[index].firstStage.brownCards, ancientsData[index].firstStage.blueCards, ancientsData[index].secondStage.greenCards, ancientsData[index].secondStage.brownCards, ancientsData[index].secondStage.blueCards, ancientsData[index].thirdStage.greenCards, ancientsData[index].thirdStage.brownCards, ancientsData[index].thirdStage.blueCards,];
+    console.log(stage1);
+    dots.forEach((elem, i) => {
+        // console.log(dots[elem].textContent)
+        // dots[elem].textContent === stage1[i]
+    })
+    const getNameGods = [];
+    const getStages = [];
 
+    //Собираю инфу объектов
+    ancientsData.forEach((elem => {
+        getNameGods.push(elem.name)
+    }))
+    console.log(getNameGods);
+
+    ancientsData.forEach((elem => {
+        getStages.push(elem.firstStage)
+    }))
+    console.log(getStages);
 }
+
+
 
 ancientGods.onclick = function (event) {
     ancientToggleClass(event);
